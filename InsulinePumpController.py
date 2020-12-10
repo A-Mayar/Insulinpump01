726e5ce9506d203e8ef4836d9b6aeee9777c00c0
A-Mayar <74905799+A-Mayar@users.noreply.github.com>
2020-12-10 03:45:17 -0800

    Add files via upload

---
+++ InsulinePumpController.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+insulin_level = 10
+insulinpump = 5
+counter = 4
+while insulin_level>=3:
+    if insulin_level<=3:
+        print("Warning! Sugar level is High")
+        insulin_level=(insulinpump-counter)*10
+        counter = counter-1
+        print(insulin_level)
+        
+        insulinpump = insulinpump - 1
+        print("Remaining insulin injection = ",insulinpump)
+        if insulinpump==1:
+            insulin_level = insulinpump*10
+            print(insulin_level,"\n","Sugar level stablised")
+            insulinpump=0
+            print("Remaining insulin injection = ",insulinpump)
+            print("Warning! Insulin out of reach......","\n","Need insulin injection...")
+            break
+    else:
+        insulin_level = insulin_level - 1
+        print(insulin_level)
